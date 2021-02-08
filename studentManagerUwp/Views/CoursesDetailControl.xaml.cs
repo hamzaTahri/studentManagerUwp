a2e1d42cef692ac91b2a01c6dc2dac594f415af7
ForUwp <ForUwp@ForUwp>
2021-02-08 20:55:21 +0000

    First Setup

---
+++ studentManagerUwp/Views/CoursesDetailControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using studentManagerUwp.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace studentManagerUwp.Views
+{
+    public sealed partial class CoursesDetailControl : UserControl
+    {
+        public SampleOrder MasterMenuItem
+        {
+            get { return GetValue(MasterMenuItemProperty) as SampleOrder; }
+            set { SetValue(MasterMenuItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(SampleOrder), typeof(CoursesDetailControl), new PropertyMetadata(null, OnMasterMenuItemPropertyChanged));
+
+        public CoursesDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnMasterMenuItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as CoursesDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
---
+++ studentManagerUwp/Views/SessionsDetailControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using studentManagerUwp.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace studentManagerUwp.Views
+{
+    public sealed partial class SessionsDetailControl : UserControl
+    {
+        public SampleOrder MasterMenuItem
+        {
+            get { return GetValue(MasterMenuItemProperty) as SampleOrder; }
+            set { SetValue(MasterMenuItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(SampleOrder), typeof(SessionsDetailControl), new PropertyMetadata(null, OnMasterMenuItemPropertyChanged));
+
+        public SessionsDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnMasterMenuItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as SessionsDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
